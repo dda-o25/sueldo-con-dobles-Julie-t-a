d963a625bdb6f5ab005dc8f55bfb669a660c8cb2
Julie-t-a <julieta.nunez@iteso.mx>
2025-09-19 01:18:18 -0600

    primer commit

--- sueldo.py
+++ sueldo.py
@@ -1,16 +1,28 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Julieta Núñez Pacheco
+19 de septiembre de 2025
+Este ejercicio calcula el sueldo semanal según las horas y
+tarifa proporcionada
 """
 
-
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+
+horas = int(input("Horas trabajadas: "))
+tarifa = int(input("Tarifa por hora: "))
 
 # Proceso
 
+if horas > 48:
+    h_extra = horas - 48
+    sh_extra = h_extra * tarifa * 2
+    sueldo = (tarifa * 48) + sh_extra
+else: 
+    h_extra = 0 
+    sh_extra = 0 
+    sueldo = tarifa * horas
 
 # Salidas
-print(salida)
+
+print("Horas extras: " + str(h_extra))
+print("Sueldo por hora extras: " + str(sh_extra))
+print("Sueldo semanal: " + str(sueldo))
